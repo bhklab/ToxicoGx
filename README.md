d248159239028c8a0cc608710f65a613875e2df3
Esther Yoo <ykesther10@gmail.com>
2019-07-03 19:18:24 -0400

    03-07-2019 tSet

---
+++ README.md
@@ -0,0 +1,3 @@
+# ToxicoGx
+A R package in-progress. Goal is to analyze the toxicity of drugs across large-scale pharmacogenomic datasets.
+
---
+++ eset.R
@@ -0,0 +1,65 @@
+# Install Affymetrix chip (cdf) package to manage data arrays of human hepatocytes. 
+### Name of package: Human Genome U133 Plus 2.0 Array, found at http://www.affymetrix.com/support/technical/byproduct.affx?product=hg-u133-plus
+install.packages("data/hgu133plus2hsensgcdf_22.0.0.tar.gz", repos = NULL, type = "source")
+
+# Install Affymetrix package from BioConductor.
+# Install PharmacoGx package from BioConductor.
+
+# Load libraries installed.
+setwd("data")
+library("affy")
+library("hgu133plus2hsensgcdf")
+library("PharmacoGx")
+library(Biobase)
+
+# Group all .CEL files in one variable.
+celfn <- list.celfiles("data/CELfiles", full.names = TRUE)
+
+#generate eset 
+generated_eset <- just.rma(filenames = celfn, verbose=TRUE, cdfname = "hgu133plus2hsensgcdf")
+##OR
+#read existing eset
+eset <- readRDS('C:/Users/amytx/OneDrive - UHN/tggates/eset.rds')
+
+#pdata
+path.sc=file.path("C:/Users/amytx/OneDrive - UHN/tggates")
+sampleinfo <- read.table(file.path(path.sc, "TGGATEsfeatureInfo.txt"), sep="\t")
+sampleinfo[sampleinfo == "" | sampleinfo == " "] <- NA
+annot <- read.csv("C:/Users/amytx/OneDrive - UHN/tggates/phenoDataCOLNAMES_DONE.csv", stringsAsFactors=FALSE, check.names=FALSE, header=TRUE, row.names=1)
+pData(eset) <- annot
+pData(eset)[,"batchid"] <- NA
+
+
+##fdata
+#identify probe names
+probes <- eset@assayData$exprs
+
+#map ENSG probes in eset to feature data file of ENSG probes only
+Featuredata <- read.csv("C:/Users/amytx/OneDrive - UHN/tggates/finalFeatureNAs.csv")
+rownames(Featuredata) <- Featuredata$X
+Featuredata <- subset(Featuredata, select = -c(X.1, X))
+Featuredata$BEST <- NA
+names(Featuredata)[3] <- "Symbol"
+fData(eset) <- Featuredata
+
+##subset out AFFX probes in eset
+neweset <- subset(eset, substr(rownames(probes), 0, 4) != "AFFX")
+#map feature data file to no-AFFX eset
+rownames(neweset) <- Featuredata$gene_id
+#created new eset with feature info and no AFFX probes
+featureEset <- neweset
+
+annotation(featureEset) <- "rna"
+
+#save the eset, read it
+saveRDS(featureEset, file='feature_eset.rds')
+final <- readRDS('feature_eset.rds')
+final
+
+#tests
+length(intersect(pData(eset)[,"cellid"] , cell$cellid))
+length(intersect(pData(eset)[,"cellid"] , cell$cellid))
+length(intersect(pData(eset)[,"cellid"] , cell$cellid))
+length(intersect(unique(sensitivityInfo$cellid) , cell$cellid))
+
+rna <- eset
