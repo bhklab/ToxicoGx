5943803b4665079434facc922432d7af30671241
Amy Tang <amytxng@gmail.com>
2019-07-23 15:00:53 -0400

    finished eset

--- Rat inVitro/esetRatVitro.R
+++ Rat inVitro/esetRatVitro.R
@@ -25,14 +25,18 @@ celFiles <- list.celfiles("data/RatInVitroCEL", full.names = TRUE)
 # Normalize CEL files (RMA method)
 esetNorm <- just.rma(filenames = celFiles, verbose = TRUE, cdfname = "rtu34rnensgcdf")
 # Save object
-saveRDS(esetNorm, file = "eset.rds")  
+saveRDS(esetNorm, file = "rds/eset.rds")  
 
 
 #-------------------------#
 ## CONTINUE HERE INSTEAD ##
 
 # Generate eset so far, ONLY includes normalized expression
-eset <- readRDS("data/eset.rds")
+eset <- readRDS("rds/eset.rds")
+# Read phenoinfo
+phenoData <- readRDS("rds/phenoData.rds")
+# Read featuredata
+featureData <- readRDS("rds/featureData.rds")
 
 # Unlock eset@assyadata to modify for desired info 
 storageMode(eset) <- "environment"
@@ -40,7 +44,25 @@ storageMode(eset) <- "environment"
 rownames(eset@protocolData@data) <- substr(rownames(eset@protocolData@data),3,12)
 colnames(eset@assayData$exprs) <- substr(colnames(eset@assayData$exprs),3,12)
 colnames(eset@assayData$se.exprs) <- substr(colnames(eset@assayData$se.exprs),3,12)
+# Subset probes
+eset <- subset(eset, substr(rownames(eset@assayData$exprs), 0, 4) != "AFFX")
+# Add phenoData
+pData(eset) <- phenoData
+# Add featureData
+fData(eset) <- featureData
+# Replace '_at' with nothing
+rownames(eset) <- gsub("_at","",rownames(eset))
+# Lock eset@assayData environment when done
+storageMode(eset)  <- "lockedEnvironment"
 
+# Set annotation info
+annotation(eset) <- "rna"
 
+# TESTS #
+#length(intersect(pData(eset)[,"cellid"] , cell$cellid))
+#length(intersect(pData(eset)[,"cellid"] , cell$cellid))
+#length(intersect(pData(eset)[,"cellid"] , cell$cellid))
+#length(intersect(unique(sensitivityInfo$cellid) , cell$cellid))
 
-
+# Save eset as .rds file
+saveRDS(eset, file="rds/eset.rds")
