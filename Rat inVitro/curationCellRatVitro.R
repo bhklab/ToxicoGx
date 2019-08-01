2c84f8746d6a93f93928f9bfd2d9977f6e4ca5c2
Amy Tang <amytxng@gmail.com>
2019-08-01 11:24:22 -0400

    create file

---
+++ Rat inVitro/curationCellRatVitro.R
@@ -0,0 +1,11 @@
+#extract from phenodata file
+phenoData <- readRDS("rds/phenoData.rds")
+#subset necessary columns
+curationCell <- subset(phenoData,select=c(samplename))
+#add new column
+curationCell$tggates.cellid <- curationCell$samplename
+#change first column name
+names(curationCell)[1] <- "unique.cellid"
+
+#save object into rds/
+saveRDS(curationCell,file="rds/curationCell.rds")
