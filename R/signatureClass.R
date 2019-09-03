950ea0b8d43d64e29a627af3c152cf65d9f5f3df
Esther <ykesther10@gmail.com>
2019-09-03 10:15:19 -0400

    Update signatureClass.R

--- R/signatureClass.R
+++ R/signatureClass.R
@@ -1,64 +1,58 @@
 setOldClass('sessionInfo', sessionInfo)
 
 #' @importFrom utils sessionInfo
-.RadioSig <- setClass('RadioSig', slots=list(
-
-            RSetName='character',
+.ToxicoSig <- setClass('ToxicoSig', slots=list(
+            Arguments = "list",
+            TSetName='character',
             DateCreated = 'character',
             SigType = 'character',
             SessionInfo = 'sessionInfo',
             Call = 'character'), contains='array')
 
-RadioSig <- function(Data=array(NA, dim=c(0,0,0)), PSetName='', DateCreated=date(), SigType='sensitivity', SessionInfo=sessionInfo(), Call='No Call Recorded'){
+ToxicoSig <- function(Data=array(NA, dim=c(0,0,0)), TSetName='', DateCreated=date(), SigType='sensitivity', SessionInfo=sessionInfo(), Call='No Call Recorded', Arguments = list()){
+  
+  #attr(SessionInfo, 'class') <- NULL
+  
+  return(.ToxicoSig(Data, Arguments = Arguments, TSetName=TSetName, DateCreated=DateCreated, SigType=SigType, SessionInfo=SessionInfo, Call=Call))}
 
-#attr(SessionInfo, 'class') <- NULL
-
-return(.RadioSig(Data, PSetName=PSetName, DateCreated=DateCreated, SigType=SigType, SessionInfo=SessionInfo, Call=Call))}
-
-
-#' Show RadioGx Signatures
-#'
+#' Show ToxicoGx Signatures  
+#' 
 #' @examples
-#' data(Cleveland_small)
-#' rad.sensitivity <- radSensitivitySig(Cleveland_small, mDataType="rna",
-#'              nthread=1, features = fNames(Cleveland_small, "rna")[1])
-#' rad.sensitivity
-#'
-#' @param object \code{RadioSig}
-#' @return Prints the RadioGx Signatures object to the output stream, and returns invisible NULL.
+#' data(TGGATES_small)
+#' drug.perturbation <- drugPerturbationSig(TGGATES_small, mDataType="rna", nthread=1, duration = 2,
+#'      drugs = head(drugNames(TGGATES_small)), features = fNames(TGGATES_small, "rna")[1])
+#' drug.perturbation
+#' 
+#' @param object \code{ToxicoSig}
+#' @return Prints the ToxicoGx Signatures object to the output stream, and returns invisible NULL. 
 #' @export
-setMethod("show", signature=signature(object='RadioSig'),
-        function(object) {
-        cat('RadioSet Name: ', attr(object, 'RSetName'), "\n")
-        cat('Signature Type: ', attr(object, 'SigType'), "\n")
-        cat("Date Created: ", attr(object, 'DateCreated'), "\n")
-        cat("Number of Radiation Types: ", dim(object)[[2]], "\n")
-        cat("Number of Genes/Probes: ", dim(object)[[1]], "\n")
-           })
+setMethod("show", signature=signature(object='ToxicoSig'),
+          function(object) {
+            cat('ToxicoSet Name: ', attr(object, 'PSetName'), "\n")
+            cat('Signature Type: ', attr(object, 'SigType'), "\n")
+            cat("Date Created: ", attr(object, 'DateCreated'), "\n")
+            cat("Number of Drugs: ", dim(object)[[2]], "\n")
+            cat("Number of Genes/Probes: ", dim(object)[[1]], "\n")
+          })
 
 #' Show the Annotations of a signature object
-#'
-#' This funtion prints out the information about the call used to compute the rad signatures, and the session info
-#' for the session in which the computation was done. Useful for determining the exact conditions used to generate signatures.
-#'
+#' 
+#' This funtion prints out the information about the call used to compute the drug signatures, and the session info 
+#' for the session in which the computation was done. Useful for determining the exact conditions used to generate signatures. 
+#' 
 #' @examples
-#' data(Cleveland_small)
-#' rad.sensitivity <- radSensitivitySig(Cleveland_small, mDataType="rna",
-#'              nthread=1, features = fNames(Cleveland_small, "rna")[1])
-#' showSigAnnot(rad.sensitivity)
-#'
-#' @param Sigs An object of the \code{RadioSig} Class, as
-#' returned by \code{radPerturbationSig} or \code{radSensitivitySig}
-#' @return Prints the RadioGx Signatures annotations to the output stream, and returns invisible NULL.
+#' data(TGGATES_small)
+#' drug.perturbation <- drugPerturbationSig(TGGATES_small, mDataType="rna", nthread=1, duration = 2,
+#'      drugs = head(drugNames(TGGATES_small)), features = fNames(TGGATES_small, "rna")[1])
+#' showSigAnnot(drug.perturbation)
+#' 
+#' @param Sigs An object of the \code{ToxicoSig} Class, as
+#' returned by \code{drugPerturbationSig}
+#' @return Prints the ToxicoGx Signatures annotations to the output stream, and returns invisible NULL. 
 #' @export
 showSigAnnot <- function(Sigs){
-
+  
   print(Sigs@Call)
   print(Sigs@SessionInfo)
   return(invisible(NULL))
-}
-
-
-
-
-
+} 
