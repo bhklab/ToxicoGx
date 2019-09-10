5b45ae9da04953fa3a5c2a47dca0b55ded438e07
sisiranair <sisira.nair@uhnresearch.ca>
2019-09-10 12:36:21 -0400

    all R codes in one folder

--- R/drugObject.R
+++
@@ -1,9 +0,0 @@
-# extract from phenodata file
-phenoData <- readRDS("data/phenoData.rds")
-#subset necessary columns
-drug <- unique(subset(phenoData,select=c(drugid, drugid_abbr, drugid_no)))
-#change rownames
-rownames(drug) <- drug$drugid
-
-#save object into data/
-saveRDS(drug,file="data/drug.rds")
