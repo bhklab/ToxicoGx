7fd683a658c8ecf98cd659dffbb13135919090a9
ChristopherEeles <christopher.eeles@outlook.com>
2019-09-26 16:53:47 -0400

    [Done] Tests for ToxicoSet class accessor methods * Next tests will be for ToxicoSet class assignment methods * Maybe I should put these all in the same file?

--- tests/testthat/test-tSetClassAccessorMethods.R
+++ tests/testthat/test-tSetClassAccessorMethods.R
@@ -1,54 +1,138 @@
-#context("Testing tSet Class Accessor Methods...")
-#
-#library(ToxicoGx)
-#library(parallel)
-#
-#test_that("@molecularProfiles slot accessors produce expected results", {
+library(testthat)
+library(ToxicoGx)
+
+#### Example tSet structure ####
+context("Testing TGGATESsmall object validity...")
+
+test_that("TGGATESsmall tSet has correct structure", {
+  data(TGGATESsmall)
+  expect_error(checkTSetStructure(TGGATESsmall), NA)
+})
+
+#### tSet Acessor methods ####
+context("Testing tSet Class Accessor Methods...")
+
+# @drug Slot
+test_that("@drug slot accessors produce expected results", {
+  data("TGGATESsmall")
+                         context("External validation...")
+  expect_equal_to_reference(drugInfo(TGGATESsmall), "drugInfo.TGGATESsmall.rds")
+  expect_equal_to_reference(drugNames(TGGATESsmall), "drugNames.TGGATESsmall.rds")
+
+  context("Internal validation...")
+  expect_equal(drugInfo(TGGATESsmall), TGGATESsmall@drug)
+  expect_equal(drugNames(TGGATESsmall), TGGATESsmall@drug$drugid)
+})
+
+# @annotation Slot
+test_that("@annotation slot accessors produce expected results", {
+  data("TGGATESsmall")
+
+  context("External validation...")
+  expect_equal_to_reference(TGGATESsmall@annotation, "annotation.TGGATESsmall.rds")
+  expect_equal_to_reference(tSetName(TGGATESsmall), "name.TGGATESsmall.rds")
+
+  context("Internal validation...")
+  expect_equal(tSetName(TGGATESsmall), TGGATESsmall@annotation$name)
+})
+
+# @molecularProfile Slot
+test_that("@molecularProfiles slot accessors produce expected results", {
+  data("TGGATESsmall")
+
+  ## TODO:: Test this with incorrect tSet structure to determine if error messages
+  ## TODO:: Write unit test for fNames()
+  # print in the correct order
+  parallel::mclapply(names(TGGATESsmall@molecularProfiles),
+                     function(name) {
+
+                       context("External validation...")
+                       expect_equal_to_reference(molecularProfiles(TGGATESsmall, name),
+                                                 paste0(name, ".molecularProfiles.TGGATESsmall.rds"))
+                       expect_equal_to_reference(featureInfo(TGGATESsmall, name),
+                                                 paste0(name, ".featureInfoa.TGGATESsmall.rds"))
+                       expect_equal_to_reference(phenoInfo(TGGATESsmall, name),
+                                                 paste0(name, ".phenoData.TGGATESsmall.rds"))
+
+                       context("Internal validation...")
+                       expect_equal(molecularProfiles(TGGATESsmall, name),
+                                    Biobase::exprs(TGGATESsmall@molecularProfiles[[name]]))
+                       expect_equal(featureInfo(TGGATESsmall, name),
+                                    Biobase::fData(TGGATESsmall@molecularProfiles[[name]]))
+                       expect_equal(phenoInfo(TGGATESsmall, name),
+                                    Biobase::pData(TGGATESsmall@molecularProfiles[[name]]))
+                       })
+})
+
+# @cell Slot
+test_that("@cell slot accessors produce expected results", {
+  data("TGGATESsmall")
+
+  context("External validation...")
+  expect_equal_to_reference(cellInfo(TGGATESsmall), "cellInfo.TGGATESsmall.rds")
+  expect_equal_to_reference(cellNames(TGGATESsmall), "cellNames.TGGATESsmall.rds")
+
+  context("Internal validation...")
+  expect_equal(cellInfo(TGGATESsmall), TGGATESsmall@cell)
+  expect_equal(cellNames(TGGATESsmall), TGGATESsmall@cell$cellid)
+})
+
+## TODO:: Implement tests for dataSetType Slot
+## @dataSetType Slot
+#test_that("@dataSetType slot accessors produce expected results", {
 #  data("TGGATESsmall")
-#  # External validation
-#  expect_equal_to_reference(featureInfo(TGGATESsmall, "rna"), "featureData.TGGATESsmall.rds")
-#  expect_equal_to_reference(phenoInfo(TGGATESsmall, "rna"), "phenoData.TGGATESsmall.rds")
-#  # Internal validation
-#  parallel::mclapply(names(TTGATES_small@molecularProfiles),
-#                     function(x) {
-#                       expect_equal(molecularProfiles(TGGATESsmall, x),
-#                                    TGGATESsmall@molecularProfiles[x])
-#                       })
-#})
-#
-#test_that("@cell slot accessors produce expected results", {
-#  data("TGGATESsmall")
-#  # External validation
+#  context("External validation...")
 #  expect_equal_to_reference(cellInfo(TGGATESsmall), "cell.TGGATESsmall.rds")
-#  # Internal validation
+#  context("Internal validation...")
 #  expect_equal(cellInfo(TGGATESsmall), TGGATESsmall@cell)
 #})
-#
-#test_that("@drug slot accessors produce expected results", {
-#  data("TGGATESsmall")
-#  # External validation
-#  expect_equal_to_reference(drugInfo(TGGATESsmall), "drug.TGGATESsmall.rds")
-#  # Internal validation
-#})
-#
-#
-#test_that("@annotation slot accessors produce expected results", {
-#  data("TGGATESsmall")
-#  # External validation
-#  expect_equal_to_reference(drugInfo(TGGATESsmall), "drug.TGGATESsmall.rds")
-#  # Internal validation
-#
-#})
 
-#test_that("", {
-#  data("TGGATESsmall")
-#
-#})
-#
-#test_that("", {
-#
-#})
-#
-#test_that("", {
-#
-#})
+# @sensitivty Slot
+test_that("@sensitivity slot accessors produce expected results", {
+  data("TGGATESsmall")
+
+  context("External validation...")
+  expect_equal_to_reference(sensitivityInfo(TGGATESsmall), "sensitivityInfo.TGGATESsmall.rds")
+  expect_equal_to_reference(sensitivityProfiles(TGGATESsmall), "sensitivitProfiles.TGGATESsmall.rds")
+  expect_equal_to_reference(sensitivityMeasures(TGGATESsmall), "sensitivityMeasures.TGGATESsmall.rds")
+  expect_equal_to_reference(sensNumber(TGGATESsmall), "sensNumber.TGGATESsmall.rds")
+
+  context("Internal validation...")
+  expect_equal(sensitivityInfo(TGGATESsmall), TGGATESsmall@sensitivity$info)
+  expect_equal(sensitivityProfiles(TGGATESsmall), TGGATESsmall@sensitivity$profiles)
+  expect_equal(sensitivityMeasures(TGGATESsmall), colnames(TGGATESsmall@sensitivity$profiles))
+  expect_equal(sensNumber(TGGATESsmall), TGGATESsmall@sensitivity$n)
+})
+
+# @perturbation Slot
+test_that("@perturbation slot accessors produce expected results", {
+  data("TGGATESsmall")
+  context("External validation...")
+  expect_equal_to_reference(TGGATESsmall@perturbation, "perturbation.TGGATESsmall.rds")
+  expect_equal_to_reference(pertNumber(TGGATESsmall), "pertNumber.TGGATESsmall.rds")
+  context("Internal validation...")
+  expect_equal(pertNumber(TGGATESsmall), TGGATESsmall@perturbation$n)
+})
+
+# @curation Slot
+test_that("@curation slot accessors produce expected results", {
+  data("TGGATESsmall")
+  context("External validation...")
+  expect_equal_to_reference(TGGATESsmall@curation, "curation.TGGATESsmall.rds")
+})
+
+# subsetTo Method
+test_that("@subsetTo slot accessors produce expected results", {
+  data("TGGATESsmall")
+
+  ## TODO:: Add unit tests for `[` subset operator
+  context("External validation...")
+  expect_equal_to_reference(subsetTo(
+    TGGATESsmall, drugs = drugNames(TGGATESsmall)[1]),
+    "subsetTo.TGGATESsmall.rds")
+})
+
+
+
+
+
