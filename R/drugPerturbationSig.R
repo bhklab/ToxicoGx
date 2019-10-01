8ba5ee884669a0ec170d8fcc15c73b9327d55766
ChristopherEeles <christopher.eeles@outlook.com>
2019-09-30 20:01:50 -0400

    [Fix] `drugPerturbationSig` returns correct stats * Functions such that summar y stats are independent of number of drugs * Error was due to failure of `subsetTo` to subset on drugs in phenoData * `subsetTo` also works correctly now
    Former-commit-id: 58b71e0d4f7f74b46f73d9b8a53ad7a9450e7e18
    Former-commit-id: 79cc3b4ed9402e5cb9cf9abc985d50f5cd6c02a3

--- R/drugPerturbationSig.R
+++ R/drugPerturbationSig.R
@@ -1,4 +1,3 @@
-#	Get Drug Perturbation Signatures from a ToxicoSet
 ###############################################################################
 ## Drug perturbation analysis
 ## create profiles before vs after drug for each drug
@@ -44,24 +43,31 @@
 #'
 #' @export
 #'
-drugPerturbationSig <- function(tSet, mDataType, drugs, cells=NULL, features, duration=NULL, nthread=1, returnValues=c("estimate","tstat", "pvalue", "fdr"), verbose=FALSE){
+drugPerturbationSig <- function(tSet, mDataType, drugs, cells, features, duration, dose, nthread=1, returnValues=c("estimate","tstat", "pvalue", "fdr"), verbose=FALSE){
 
-  if(is.null(duration)) {
-    duration <- unique(tSet@molecularProfiles$rna@phenoData$duration)
-  }
+  # ENSG00000198431_at   -3.403147e-10 -1.313231e-10 -1.064461e-08  3.647450e-12
+  # ENSG00000198431_at   -7.309412e-09  9.118625e-11 -1.050585e-09
 
+
+  # ALLOCATE CORES FOR PARALLEL PROCESSING
   availcore <- parallel::detectCores()
   if ( nthread > availcore) {
     nthread <- availcore
   }
   options("mc.cores"=nthread)
 
+  # DEAL WITH MISSING PARAMETERS
   if(!missing(cells)){
     if(!all(cells%in%cellNames(tSet))){
       stop("The cell names should match to the names used in cellNames(tSet)")
     }
     tSet <- subsetTo(tSet, cells=cells)
   }
+
+  #if (missing(cells) {
+  #  cells <- cellNames(tSet)
+  #}
+
   if (mDataType %in% names(tSet@molecularProfiles)) {
     #eset <- tSet@molecularProfiles[[mDataType]]
     if(Biobase::annotation(tSet@molecularProfiles[[mDataType]])!="rna"){
@@ -96,21 +102,45 @@ drugPerturbationSig <- function(tSet, mDataType, drugs, cells=NULL, features, du
     features <- features[fix]
   }
 
+  if (missing(duration)) { # Include all durations
+    duration <- unique(phenoInfo(tSet, mDataType)$duration)
+  }
+
+  if (missing(dose)) { # Include all dose levels
+    dose <- unique(phenoInfo(tSet, mDataType)$dose)
+  }
+
+  # ERROR HANDLING FOR PARAMETERS
+  #.checkParamsForErrors(tSet, mDataType, drugs, cells, features, duration, dose)
+
+
+  # SUBSET tSET BASED ON PARAMETERS
   samples <- rownames(phenoInfo(
-    subsetTo(tSet, mDataType = "rna", cells = cells, drugs = drugs, duration = duration),
-    mDataType = "rna"
+    subsetTo(tSet, mDataType = mDataType, cells = cells, drugs = drugs,
+             duration = duration, dose=dose),
+    mDataType = mDataType
   ))
+  print(samples)
+
+  #samples <- rownames(phenoInfo(tSet, mDataType)[phenoInfo(tSet, mDataType)[,"duration"] %in% duration & phenoInfo(tSet, mDataType)[,"drugid"] %in% drugs,])
+  #print(samples)
+
   # splitix <- parallel::splitIndices(nx=length(drugn), ncl=nthread)
   # splitix <- splitix[sapply(splitix, length) > 0]
-  samples_test <<- samples
 
-  # Loop of drugs to calculate summary stats
+
+  # LOOP OVER DRUGS TO CALCULATE PER DRUG SUMMARY STATISTICS
+  ## TODO:: Determine if this is supposed to be parallelized?
   mcres <- lapply(drugn, function(x, exprs, sampleinfo) {
 
+    # Subset to correct drugs
+    exprs <- exprs[which(sampleinfo[ , "drugid"] %in% x),]
+    sampleinfo <- sampleinfo[which(sampleinfo[ , "drugid"] %in% x),]
+
     res <- NULL
-    i = x
+    i <- x
     ## using a linear model (x ~ concentration + cell + batch + duration)
-    res <- ToxicoGx:::rankGeneDrugPerturbation(data=exprs, drug=i, drug.id=as.character(sampleinfo[ , "drugid"]),
+    res <- ToxicoGx:::rankGeneDrugPerturbation(data=exprs, drug=x, drug.id=as.character(sampleinfo[ , "drugid"]),
                                     drug.concentration=as.numeric(sampleinfo[ , "concentration"]),
                                     type=as.character(sampleinfo[ , "cellid"]), xp=as.character(sampleinfo[ , "xptype"]),
                                     batch=as.character(sampleinfo[ , "batchid"]),
@@ -124,12 +154,11 @@ drugPerturbationSig <- function(tSet, mDataType, drugs, cells=NULL, features, du
      sampleinfo=phenoInfo(tSet, mDataType)[phenoInfo(tSet, mDataType)[,"duration"] %in% duration & phenoInfo(tSet, mDataType)[,"drugid"] %in% drugs,]
   )
 
-
-
+  # ASSEMBLE RESULTS TO BE INCLUDED IN TOXICOSIG
   res <- do.call(c, mcres)
   res <- res[!sapply(res, is.null)]
   drug.perturbation <- array(NA, dim=c(nrow(featureInfo(tSet, mDataType)[features,, drop=FALSE]), length(res), ncol(res[[1]])), dimnames=list(rownames(featureInfo(tSet, mDataType)[features,,drop=FALSE]), names(res), colnames(res[[1]])))
-  for(j in 1:ncol(res[[1]])) {
+  for(j in seq_len(ncol(res[[1]]))) {
     ttt <- sapply(res, function(x, j, k) {
       xx <- array(NA, dim=length(k), dimnames=list(k))
       xx[rownames(x)] <- x[ , j, drop=FALSE]
@@ -138,7 +167,47 @@ drugPerturbationSig <- function(tSet, mDataType, drugs, cells=NULL, features, du
     drug.perturbation[rownames(featureInfo(tSet, mDataType)[features,, drop=FALSE]), names(res), j] <- ttt
   }
 
+  # CREATE TOXICOSIG OBJECT
   drug.perturbation <- ToxicoGx:::ToxicoSig(drug.perturbation, tSetName = cSetName(tSet), Call = as.character(match.call()), SigType='Perturbation')
 
+  # RETURN TOXICOSIG OBJECT
   return(drug.perturbation)
+}
+
+## TODO:: Determine if these are the correct error criteria for this function
+# Generates a descriptive error message if parameter input doesn't meet the function criteria
+.checkParamsForErrors <- function(tSet, mDataType, cell.lines, drugs, features, duration, dose) {
+
+  # tSet checks
+  ifelse(length(unlist(tSet)) > 1, stop("You may only pass in one tSet."), "" ) -> pass # This prevents print if the test doesn't error
+  # mDataType checks
+  ifelse(length(unlist(mDataType)) > 1, stop("Please only pass in one molecular data type."), "" ) -> pass
+  ifelse(!is.character(mDataType), stop("mDataType must be a string."), "" ) -> pass
+  ifelse(all(!(mDataNames(unlist(tSet)) %in% mDataType)),
+         stop(paste0("The molecular data type(s) ",
+                     paste(mDataType[which(!(mDataType %in% mDataNames(tSet)))], collapse = ", " ),
+                     " is/are not present in ", tSet@annotation$name, ".")), "") -> pass
+  # cell.lines checks
+  ifelse(!is.character(unlist(cell.lines)) ~ stop("cell.lines parameter must contain strings."), "") -> pass
+  ifelse(all(!(cellNames(tSet) %in% cell.lines)) ~ stop(paste0("The cell line(s) ",
+                                                               paste(cell.lines[which(!(cell.lines %in% cellNames(tSet)))], collapse = ", "),
+                                                               " is/are not present in ", tSet@annotation$name, "."))) -> pass
+  # drugs checks
+  ifelse(!is.character(unlist(drugs)) ~ stop("drugs parameter must contain strings.")) -> pass
+  ifelse(all(!(drugNames(tSet) %in% drugs)) ~ stop(paste0("The drug(s) ",
+                                                          paste(drugs[which(!(drugs %in% drugNames(tSet)))], collapse = ", "),
+                                                          " is/are not present in ", tSet@annotation$name, ".")), "") -> pass
+  # features checks
+  ifelse(!is.character(unlist(features)) ~ stop("features parameter contain strings."), "") -> pass
+  iflese(all(!(fNames(tSet, mDataType[1]) %in% features)) ~ stop(paste0("The feature(s) ",
+                                                                        paste(features[which(!(features %in% fNames(tSet, mDataType[1])))], collapse = ", "),
+                                                                        " is/are not present in ", tSet@annotation$name, ".")), "") -> pass
+  # duration checks
+  ifelse(!is.character(unlist(duration)) ~ stop("duration parameter must contain strings."), "") -> pass
+  ifelse(all(!(sensitivityInfo(tSet)$duration_h %in% duration)) ~ stop(paste0("The duration(s) ",
+                                                                              paste(duration[which(!(duration %in% sensitivityInfo(tSet)$duration_h))]), collapse = ", ",
+                                                                              "is/are not present in ", tSet@annotation$name, ".")), "") -> pass
+  # dose checks
+  ifelse(all(!(phenoInfo(TGGATESsmall, mDataType)$dose_levels %in% dose)),
+         stop(paste0("The molecular data type ", mDataType, " is not present in ", tSet@annotation$name, " with these parameters.")), "") -> pass
 }
