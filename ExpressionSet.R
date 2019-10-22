6586e5a1a9ed9a5f196d7244383b1530b5a0bc84
Parwaiz Nijrabi <parwaiznijrabi@Sisiras-iMac.local>
2019-10-22 19:20:39 -0400

    next steps and affy install

--- ExpressionSet.R
+++ ExpressionSet.R
@@ -5,7 +5,7 @@ setwd("~Desktop/practicedata")
 
 #phenodata
 phenodata <- read.csv(file = "s_Hepatocyte.csv", head = TRUE, sep="\t")
-phenodata
+aphenodata
 hep.filename <- sort(Hep.Metadata$Characteristics.Subject.ID., decreasing=FALSE,)
 
 is.vector(data$Characteristics.Subject.ID.)
@@ -17,4 +17,9 @@ unique(phenodata$Factor.Value.Compound.)
 paste0(hep.filename,".CEL")
 
 #assaydata 
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
 
+BiocManager::install("affy")
+
+browseVignettes("affy")
