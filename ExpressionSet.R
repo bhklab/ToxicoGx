5efd0bcdb01715d83b95e82dddc39f76dd7c2508
Parwaiz Nijrabi <parwaiznijrabi@Sisiras-iMac.uhn.ca>
2019-10-22 17:35:08 -0400

    Cleared ToxicoGx code files

---
+++ ExpressionSet.R
@@ -0,0 +1,20 @@
+library(ToxicoGx)
+setwd("~Desktop/practicedata/")
+setwd("~Desktop/practicedata")
+
+
+#phenodata
+phenodata <- read.csv(file = "s_Hepatocyte.csv", head = TRUE, sep="\t")
+phenodata
+hep.filename <- sort(Hep.Metadata$Characteristics.Subject.ID., decreasing=FALSE,)
+
+is.vector(data$Characteristics.Subject.ID.)
+
+phenodata$Factor.Value.Compound.
+
+unique(phenodata$Factor.Value.Compound.)
+
+paste0(hep.filename,".CEL")
+
+#assaydata 
+
