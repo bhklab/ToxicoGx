707328a95b43a5133dbe0b05d3f21e5b64e697bb
ChristopherEeles <christopher.eeles@outlook.com>
2019-10-29 18:01:37 -0400

    Add: downloadTSet function working
    Former-commit-id: 028fcfdc54e4e6df4c2b15564c6d6ec7b14ec93f
    Former-commit-id: c9bd5df256713f47d861dc82f9d3c1c0ece1c1f5
    Former-commit-id: 0d89d13054621b9ef18035348d4d03ba89329882

---
+++ R/downloadTSet.R
@@ -0,0 +1,111 @@
+#' Return a table of ToxicoSets available for download
+#'
+#' The function fetches a table of all ToxicoSets available for download from
+#' the PharmacoGx server. The table includes the names of the PharamcoSet, the
+#' types of data available in the object, and the date of last update.
+#'
+#' @examples
+#' if (interactive()){
+#' availabletSets()
+#' }
+#'
+#' @param saveDir \code{character} Directory to save the table of tSets
+#' @param myfn \code{character} The filename for the table of tSets
+#' @param verbose \code{bool} Should status messages be printed during download.
+#' @return A data.frame with details about the available ToxicoSet objects
+#' @export
+#' @import downloader
+#' @importFrom utils read.table write.table
+availabletSets <- function(saveDir=file.path(".", "tSets"), myfn="availableToxicoSets.csv", verbose=TRUE){
+
+  if (!file.exists(saveDir)) {
+    dir.create(saveDir, recursive = TRUE)
+  }
+
+  tryCatch({
+    downloader::download("https://ndownloader.figshare.com/files/18155444?private_link=d286d7386d5f5e778585",
+                         destfile = file.path(saveDir, myfn),
+                         quiet = !verbose)
+  }, warning = function(w) {
+    message(paste(myfn ,"download has a warning, this is likely due to connectivity issues."))
+  }, error = function(e) {
+    message(paste(myfn, "download has errored, this is likely due to server availability issues and should be resolved shortly."))
+  }, finally = {
+    message(paste(myfn, "downloaded successfully to", saveDir))
+  })
+
+  tSetTable <- read.csv(file.path(saveDir, myfn), header = TRUE, stringsAsFactors = FALSE)
+  return(tSetTable)
+}
+
+
+#' Download a ToxicoSet object
+#'
+#' This function allows you to download a \code{ToxicoSet} object for use with this
+#' package. The \code{ToxicoSets} have been extensively curated and organised within
+#' a PharacoSet class, enabling use with all the analysis tools provided in
+#' \code{PharmacoGx}.
+#'
+#' @examples
+#' if (interactive()){
+#' downloadtSet("TGGATESvignette")
+#' }
+#'
+#' @param name \code{Character} string, the name of the PhamracoSet to download.
+#' @param saveDir \code{Character} string with the folder path where the
+#'     ToxicoSet should be saved. Defaults to \code{'./tSets/'}. Will create
+#'     directory if it does not exist.
+#' @param tSetFileName \code{character} string, the file name to save the dataset under
+#' @param verbose \code{bool} Should status messages be printed during download.
+#'   Defaults to TRUE.
+#' @return A tSet object with the dataset, downloaded from our server
+#' @export
+#' @import downloader
+downloadtSet <- function(name, saveDir = file.path(".", "tSets"), tSetFileName = NULL, verbose = TRUE) {
+
+  tSetTable <- availabletSets(saveDir = saveDir)
+
+  whichx <- match(name, tSetTable[,1])
+  if (is.na(whichx)) {
+    stop('Unknown Dataset. Please use the availabletSets() function for the table of available PharamcoSets.')
+  }
+
+  if (!file.exists(saveDir)) {
+    dir.create(saveDir, recursive = TRUE)
+  }
+
+  if (is.null(tSetFileName)){
+    tSetFileName <- paste(tSetTable[whichx,"tSet.Name"], ".RData", sep="")
+  }
+  if (!file.exists(file.path(saveDir, tSetFileName))) {
+    tryCatch({
+        downloader::download(url = as.character(tSetTable[whichx,"URL"]), destfile = file.path(saveDir, tSetFileName), quiet = !verbose)
+      }, warning = function(w) {
+        message(paste(tSetFileName, "download has a warning, this is likely due to connectivity issues."))
+      }, error = function(e) {
+        message(paste(tSetFileName, "download has errored, this is likely due to server availability issues and will be resolved shortly."))
+      }, finally = {
+        message(paste(tSetFileName, "downloaded successfully to", saveDir))
+      })
+    }
+  tSet <- load(file.path(saveDir, tSetFileName))
+  return(get(tSet))
+}
+
+#' @importFrom utils read.table write.table
+.createtSetEntry <- function(tSet, outfn) {
+
+  if (file.exists(outfn)) {
+    tSetTable <- read.table(outfn, as.is = TRUE)
+    newrow <- c(tSetName(tSet), tSet@datasetType, paste(names(tSet@molecularProfiles), collapse = "/"), tSet@annotation$dateCreated, NA)
+    tSetTable <- rbind(tSetTable, newrow)
+    rownames(tSetTable) <- tSetTable[,1]
+    write.table(tSetTable, file = outfn)
+  } else {
+    newrow <- c(tSetName(tSet), tSet@datasetType, paste(names(tSet@molecularProfiles), collapse = "/"), tSet@annotation$dateCreated, NA)
+    tSetTable <- t(matrix(newrow))
+    colnames(tSetTable) <- c("tSet.Name","Description", "Available.Molecular.Profiles","Date.Updated","URL")
+    rownames(tSetTable) <- tSetTable[,1]
+    write.table(tSetTable, file = outfn)
+  }
+}
