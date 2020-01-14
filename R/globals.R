54d85694f998d387b469c1d417d2b235ce1c7799
Christopher Eeles <christopher.eeles@outlook.com>
2020-01-14 17:01:09 -0500

    Update: Resubmitted package to CRAN
    Former-commit-id: c4c882a51d8f4e422bd2bb9573c5fe97f89035ce
    Former-commit-id: 4141da5ecce23275b990fb9e91ac0015be9e8591
    Former-commit-id: e8f9c5aa2f4cb39f7eb5b9d1e13b644c9dbb5181

---
+++ R/globals.R
@@ -0,0 +1,6 @@
+# Declaring global variables for dplyr and data.table column names
+
+utils::globalVariables(c('cellid','drugid','read.csv','samplename','.',
+                         'Symbol', 'feature', 'Control', 'Low', 'Middle',
+                         'High', 'verbose', 'dose_level', 'individual_id',
+                         'duration_h', 'viability'))
