077477ab7b0d9884b079dda086c14d590713a47f
ChristopherEeles <christopher.eeles@outlook.com>
2021-05-12 22:41:04 -0400

    Fix: Passing BiocCheck with no warnings and new CoreGx installed

--- R/allGenerics.R
+++ R/allGenerics.R
@@ -1,16 +1,3 @@
-### FIXME:: Move this into CoreGx
-##' Private getter for .intern environment slot inside an object.
-##'
-##' @param object What to object to retrieve a value from .intern for
-##' @parma ... Allow definition of new parmaters on this generic
-##'
-##' @keywords internal
-##' @exprot
-##' @noRd
-#setGeneric('.getIntern', function(object, ...)
-#    standardGeneric('.getIntern'))
-
-## TODO:: Move this to CoreGx
 #' Generic method for performing differential expression analysis on an S4 object
 #'   using the limma package
 #'
